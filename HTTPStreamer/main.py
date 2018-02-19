d7711ec4ec8a0498baa5d6bf025249fc00fa8372
pityhero233 <shizhengyu93@hotmail.com>
2018-02-19 18:41:17 +0800

    still use mjpg-streamer for video. modified the source file.

--- HTTPStreamer/main.py
+++
@@ -1,4 +0,0 @@
-import os
-
-res=os.system('''./mjpg-streamer/mjpg-streamer-experimental/mjpg_streamer -i "input_uvc.so -f 10 -y" -o "output_http.so -c "pityhero:pityhero:" -w www -p 8888" -o "output_file.so -d 1000 -f /home/void/projects/Petty/HTTPStreamer/cache/"''')
-
--- src/main.py
+++ src/main.py
@@ -1,55 +1,63 @@
+#!/usr/bin/python
 import cv2
 import numpy as np
 import time
-#import os.system as sys
 import os
+import thread
 import string
+
 lower = np.array([40,0,0])
 upper = np.array([85,255,255])
 LowerBlue = np.array([100, 0, 0])
 UpperBlue = np.array([130, 255, 255])
 iBest = -1.0
+
+def start_service():
+    res=os.system('''./mjpg_streamer -i "input_uvc.so -d /dev/video1 -f 10 -y" -o "output_http.so -w www -p 8888"''')
+
+print "step 1:start camera..."
 try:
-    cam = cv2.VideoCapture(1)
     cam2 = cv2.VideoCapture(0)
     cv2.namedWindow("splitter",cv2.WINDOW_AUTOSIZE)
 except:
     print "E:cam error"
     system._exit()
 count = 0
+print "step 2:start video service..."
+try:
+    thread.start_new_thread(start_service,())
+except:
+    print "New Thread Error"
+    os._exit()
+
 #-------------------------------
-while count < 1000:
+while True:
     count = count + 1
-    #print "Test #",count,":"
     try:
         _,frame2 = cam2.read()
-        _,frame = cam.read()
-    #cv2.namedWindow("splitter",cv2.WINDOW_AUTOSIZE)
+        cv2.namedWindow("splitter",cv2.WINDOW_AUTOSIZE)
     except aa:
         print("E:get frame error.")
         print(aa.message)
         os._exit()
-#es = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (10,10))
     element = cv2.getStructuringElement(cv2.MORPH_RECT,(5,5))
 
     HSV =  cv2.cvtColor(frame2,cv2.COLOR_BGR2HSV)
-
-    #HSV = cv2.GaussianBlur(HSV, (5, 5), 0)
     H,S,V = cv2.split(HSV)
     mask = cv2.inRange(HSV,lower,upper)
     certain = cv2.bitwise_and(frame2,frame2,mask=mask)
-    #cv2.imshow("splitter",np.stack([frame,frame2,certain]))
+
     diff = cv2.GaussianBlur(certain,(5,5),0)
     diff = cv2.threshold(certain, 25, 255, cv2.THRESH_BINARY)[1]
     diff = cv2.morphologyEx(diff,cv2.MORPH_OPEN,element)
     diff = cv2.morphologyEx(diff,cv2.MORPH_CLOSE,element)
 
     gray = cv2.cvtColor(frame2,cv2.COLOR_BGR2GRAY)
-#print(frame2.shape)
 
-#circles1= cv2.HoughCircles(gray,cv2.HOUGH_GRADIENT,1,100,param1=100,param2=30,minRadius=5,maxRadius=240)
+
+
     circles1 = cv2.HoughCircles(gray,cv2.HOUGH_GRADIENT,1,150,param1=100,param2=30,minRadius=15,maxRadius=100)
-#circles2 = cv2.HoughCircles(diff,cv2.HOUGH_GRADIENT,1,100,param1=100,param2=30,minRadius=5,maxRadius=300)
+
     try:
         circlesm = circles1[0,:,:]
         circlesm = np.uint16(np.around(circlesm))
@@ -79,11 +87,8 @@ while count < 1000:
                 iBest = index
 
     except TypeError,e:
-        #print("no circle detected!")
         pass
     if (iBest!=-1):
-        #print "Best circle chosen @i=",iBest,",conf=",bestConf,",R=",circlesm[iBest][2]
-
         try:
             cv2.circle(frame2,(circlesm[iBest][0],circlesm[iBest][1]),circlesm[iBest][2],(0,255,0),5)
             #name = "test#"+str(count)+".jpg"
