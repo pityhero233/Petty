f72c58e0bb783f1997e96c42eb7b2d2bfa971ed1
pityhero233 <shizhengyu93@hotmail.com>
2018-02-20 20:09:41 +0800

    it just down.

--- PettyAndroid/app/src/main/java/com/hzxjzx/xjrobotics/pettyandroid/MainActivity.java
+++ PettyAndroid/app/src/main/java/com/hzxjzx/xjrobotics/pettyandroid/MainActivity.java
@@ -5,14 +5,23 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG="MainActivity";
-//    private void InputDialog(){
+    public boolean automode = false;
+    public MenuItem gMenuItem=null;
+    public String add;
+    public String foreAddress;
+    public WebView server = findViewById(R.id.sender);
+    //    private void InputDialog(){
 //        final EditText inputServer = new EditText(this);
 //        inputServer.setFocusable(true);
 //
@@ -34,13 +43,14 @@ public class MainActivity extends AppCompatActivity {
     editText.setText(getText(R.string.ipdefault));
     AlertDialog.Builder inputDialog =
             new AlertDialog.Builder(MainActivity.this);
-    inputDialog.setTitle("请输入ip:").setView(editText);
+    inputDialog.setTitle("请输入识别码:").setView(editText);
     inputDialog.setPositiveButton("确定",
             new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialog, int which) {
                     String input = editText.getText().toString();
-                    String add = "http://192.168.0."+input+":8888/?action=stream";
+                    add = "http://192.168.0."+input+":8888/?action=stream";
+                    foreAddress = "http://192.168.0."+input+"/controls/";
                     WebView a = (WebView) findViewById(R.id.webview_1);
                     a.loadUrl(add);
                     Toast.makeText(MainActivity.this,add,Toast.LENGTH_SHORT).show();
@@ -48,6 +58,42 @@ public class MainActivity extends AppCompatActivity {
                 }
             }).show();
 }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.refresh:
+                WebView a = findViewById(R.id.webview_1);a.loadUrl(add);
+                Toast.makeText(MainActivity.this, "刷新成功", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.exit:
+                finish();
+                break;
+            case R.id.automode:
+                assert gMenuItem!=null;
+                if (automode){
+                    gMenuItem.setTitle("启动自动模式");
+                    server.loadUrl(foreAddress+"ad");
+                    automode=false;
+                    Toast.makeText(MainActivity.this,"自动模式已关闭！",Toast.LENGTH_SHORT);
+                }else{
+                    gMenuItem.setTitle("关闭自动模式");
+                    server.loadUrl(foreAddress+"au");
+                    automode=true;
+                    Toast.makeText(MainActivity.this,"自动模式已开启！",Toast.LENGTH_SHORT);
+                }
+            default:
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main,menu);
+        gMenuItem = menu.findItem(R.id.automode);
+        return true;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -56,6 +102,34 @@ public class MainActivity extends AppCompatActivity {
         WebView a = findViewById(R.id.webview_1);
         a.getSettings().setJavaScriptEnabled(true);
         a.setWebViewClient(new WebViewClient());
+//        Button u = (Button) findViewById(R.id.floatingActionButton4);
+//        Button d = (Button) findViewById(R.id.floatingActionButton6);
+//        Button l = (Button) findViewById(R.id.floatingActionButton5);
+//        Button r = (Button) findViewById(R.id.floatingActionButton3);
+//        u.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View v){
+//                server.loadUrl(foreAddress+"f");
+//            }
+//        });
+//        d.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                server.loadUrl(foreAddress+"b");
+//            }
+//        });
+//        l.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                server.loadUrl(foreAddress+"l");
+//            }
+//        });
+//        r.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                server.loadUrl(foreAddress+"r");
+//            }
+//        });
         Log.d(TAG,"InputDialog Exec");
         showInputDialog();
         Log.d(TAG,"Exec complete.");
