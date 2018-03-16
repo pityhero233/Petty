9e662b766f202e93c3618a5f17c7961c04aedb04
pityhero233 <shizhengyu93@hotmail.com>
2018-03-16 18:40:42 +0800

    fix tiny bug for #2 NXT contour

--- BallDetector/src/main.py
+++ BallDetector/src/main.py
@@ -37,13 +37,13 @@ while True:
 	cnts = cv2.findContours(mask.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)[-2]
 	center = None
 	if len(cnts)>0:
-    	c = max(cnts,key=cv2.contourArea)
-    	((x,y),radius) = cv2.minEnclosingCircle(c)
-    	M=cv2.moments(c)
-    	center = (int(M["m10"]/M["m00"]), int(M["m01"] / M["m00"]))
-    	if radius > 10:
-        	cv2.circle(frame2,(int(x),int(y)),int(radius),(0,255,255),2)
-        	cv2.circle(frame2,center,5,(0,0,255),-1)
+    		c = max(cnts,key=cv2.contourArea)
+    		((x,y),radius) = cv2.minEnclosingCircle(c)
+    		M=cv2.moments(c)
+    		center = (int(M["m10"]/M["m00"]), int(M["m01"] / M["m00"]))
+    		if radius > 10:
+        		cv2.circle(frame2,(int(x),int(y)),int(radius),(0,255,255),2)
+        		cv2.circle(frame2,center,5,(0,0,255),-1)
 
 
 	gray = cv2.cvtColor(frame2,cv2.COLOR_BGR2GRAY)
