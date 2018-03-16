db4452fe482153da08d95979a1ddacc5edceb819
pityhero233 <shizhengyu93@hotmail.com>
2018-03-16 18:38:00 +0800

    add contour to main.py at beta NXT(2).

--- BallDetector/src/main.py
+++ BallDetector/src/main.py
@@ -1,113 +1,55 @@
 import cv2
 import numpy as np
-import time
 #import os.system as sys
-import os
-import string
 
+import os
 #lower = np.array([40,0,0])
 #upper = np.array([85,255,255])
-lower = (29,86,6)
+lower = (25,86,6)
 upper = (64,255,255)
-
 LowerBlue = np.array([100, 0, 0])
 UpperBlue = np.array([130, 255, 255])
 iBest = -1.0
 try:
-    cam = cv2.VideoCapture(1)
-    cam2 = cv2.VideoCapture(0)
+    cam2 = cv2.VideoCapture(1)
     cv2.namedWindow("splitter",cv2.WINDOW_AUTOSIZE)
-except:
-    print "E:cam error"
-    system._exit()
-count = 0
-#-------------------------------
-while count < 1000:
-    count = count + 1
-    #print "Test #",count,":"
-    try:
-        _,frame2 = cam2.read()
-        _,frame = cam.read()
-    #cv2.namedWindow("splitter",cv2.WINDOW_AUTOSIZE)
-    except aa:
-        print("E:get frame error.")
-        print(aa.message)
-        os._exit()
+except aa:
+    print(aa.message)
+    os._exit()
 #es = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (10,10))
-    element = cv2.getStructuringElement(cv2.MORPH_RECT,(5,5))
+element = cv2.getStructuringElement(cv2.MORPH_RECT,(5,5))
 
-    HSV =  cv2.cvtColor(frame2,cv2.COLOR_BGR2HSV)
+while True:
+	print "fine."
+	_,frame2 = cam2.read()
+	HSV =  cv2.cvtColor(frame2,cv2.COLOR_BGR2HSV)
+	#HSV = cv2.GaussianBlur(HSV, (5, 5), 0)
+	H,S,V = cv2.split(HSV)
+	mask = cv2.inRange(HSV,lower,upper)
+	mask = cv2.erode(mask, None, iterations=2)
+	mask = cv2.dilate(mask, None, iterations=2)
+	certain = cv2.bitwise_and(frame2,frame2,mask=mask)
+	#cv2.imshow("splitter",np.stack([frame,frame2,certain]))
+	diff = cv2.GaussianBlur(certain,(5,5),0)
+	diff = cv2.threshold(certain, 25, 255, cv2.THRESH_BINARY)[1]
+	diff = cv2.morphologyEx(diff,cv2.MORPH_OPEN,element)
+	diff = cv2.morphologyEx(diff,cv2.MORPH_CLOSE,element)
+	cnts = cv2.findContours(mask.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)[-2]
+	center = None
+	if len(cnts)>0:
+    	c = max(cnts,key=cv2.contourArea)
+    	((x,y),radius) = cv2.minEnclosingCircle(c)
+    	M=cv2.moments(c)
+    	center = (int(M["m10"]/M["m00"]), int(M["m01"] / M["m00"]))
+    	if radius > 10:
+        	cv2.circle(frame2,(int(x),int(y)),int(radius),(0,255,255),2)
+        	cv2.circle(frame2,center,5,(0,0,255),-1)
 
-    #HSV = cv2.GaussianBlur(HSV, (5, 5), 0)
-    #H,S,V = cv2.split(HSV)
-    mask = cv2.inRange(HSV,lower,upper)
-    #certain = cv2.bitwise_and(frame2,frame2,mask=mask)
-    mask = cv2.erode(mask,None,iterations=2)
-    mask = cv2.dilate(mask,None,iterations=2)
 
-    #cv2.imshow("splitter",np.stack([frame,frame2,certain]))
-    #diff = cv2.GaussianBlur(certain,(5,5),0)
-    #diff = cv2.threshold(certain, 25, 255, cv2.THRESH_BINARY)[1]
-    #diff = cv2.morphologyEx(diff,cv2.MORPH_OPEN,element)
-    #diff = cv2.morphologyEx(diff,cv2.MORPH_CLOSE,element)
+	gray = cv2.cvtColor(frame2,cv2.COLOR_BGR2GRAY)
+	print(frame2.shape)
 
-    gray = cv2.cvtColor(frame2,cv2.COLOR_BGR2GRAY)
-#print(frame2.shape)
+	cv2.imshow("splitter",np.hstack([frame2,certain]))
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
-#circles1= cv2.HoughCircles(gray,cv2.cv.CV_HOUGH_GRADIENT,1,100,param1=100,param2=30,minRadius=5,maxRadius=240)
-    circles1 = cv2.HoughCircles(gray,cv2.cv.CV_HOUGH_GRADIENT,1,150,param1=100,param2=30,minRadius=15,maxRadius=100)
-#circles2 = cv2.HoughCircles(diff,cv2.HOUGH_GRADIENT,1,100,param1=100,param2=30,minRadius=5,maxRadius=300)
-    try:
-        circlesm = circles1[0,:,:]
-        circlesm = np.uint16(np.around(circlesm))
-        bestConf = -1
-        for index,i in enumerate(circlesm[:]):
-            cv2.circle(frame2,(i[0],i[1]),i[2],(255,0,0),5)
-            cv2.circle(certain,(i[0],i[1]),i[2],(255,0,0),5)
-            cv2.circle(diff,(i[0],i[1]),i[2],(255,0,0),5)
-        #print "circle:",i[0],"x",i[1],",r=",i[2]
-            bx = i[0]-i[2];by = i[1]-i[2];
-            ex = i[0]+i[2];ey = i[1]+i[2];
-            confident = 0;
-            for x in range(bx,ex):
-                for y in range(by,ey):
-                    try:
-                        for vector in diff[y,x,:]:
-                            if vector >125:
-                                confident = confident + 1
-                    except IndexError,e:
-                        pass
-                        #print e.message;
-                        #print("W:Index read error\n")
-            confident = confident*1000.0/(4.0*i[2]*i[2])
-            #print "confidence = ",confident
-            if bestConf<confident:
-                bestConf = confident
-                iBest = index
-
-    except TypeError,e:
-        #print("no circle detected!")
-        pass
-    if (iBest!=-1):
-        #print "Best circle chosen @i=",iBest,",conf=",bestConf,",R=",circlesm[iBest][2]
-
-        try:
-            cv2.circle(frame2,(circlesm[iBest][0],circlesm[iBest][1]),circlesm[iBest][2],(0,255,0),5)
-            #name = "test#"+str(count)+".jpg"
-            #name2 = "test#"+str(count)+"c.jpg"
-            #cv2.imwrite(name,frame2);
-            #cv2.imwrite(name2,diff)
-            cv2.imshow("splitter",np.hstack([frame2,diff]))
-            cv2.waitKey(10)
-            pass
-        except TypeError,e:
-            #print e.message
-            pass
-            #print "E:Segmentation Fault while choosing iBest"
-    else:
-        pass
-        #print "no best circle chosen..."
-    #cv2.imshow("splitter",np.hstack([frame2,certain]))
-    #cv2.waitKey(0)
-    #cv2.destroyAllWindows()
-    #time.sleep(1)
